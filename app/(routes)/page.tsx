8713e35e61893a044eb7465e2b87383e32bf6c70
Matheus <matheusdaws66@gmail.com>
2024-01-16 16:52:42 -0300

    Inicio frontend

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,7 @@
+const HomePage = () =>{
+    return (
+        <div>HelloStore</div>
+    )
+}
+
+export default HomePage;
---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+const Footer =() => {
+    return (
+        <footer className="bg-wgite border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                &copy; 2024 TeteuStore. Todos os direitos reservados.
+
+                </p>
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
